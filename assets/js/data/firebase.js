89d6618b4cbfb3da8abae22ffa374559f0c02d26
Wendel Santos <wendel-020@live.com>
2026-01-05 17:36:58 -0300

    Portifolio pronto

---
+++ assets/js/data/firebase.js
@@ -0,0 +1,5 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js'
+import firebaseConfig from './config.js'
+
+const app = initializeApp(firebaseConfig)
+export default app
